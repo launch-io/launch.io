02fe109c5e16d11e82fac60c1b7f828ca28e3230
Andrew Petersen <andrew.petersen15@gmail.com>
2020-10-03 13:00:17 -0600

    fixed bug in useLaunchSelector

--- example/src/features/calculator/calculator.hooks.ts
+++ example/src/features/calculator/calculator.hooks.ts
@@ -1,13 +1,11 @@
-import { useMemo } from "react";
 import { useLaunchSelector } from "launch.io";
 
 export const useCalculator = () => {
   const actions = useLaunchSelector(({ actions }) => actions.calculator);
   const state: any = useLaunchSelector(({ state }) => state.calculator);
-  return useMemo(() => {
-    return {
-      ...state,
-      ...actions,
-    };
-  }, [state, actions]);
+
+  return {
+    ...state,
+    ...actions,
+  };
 };
--- src/hooks/useLaunchSelector.ts
+++ src/hooks/useLaunchSelector.ts
@@ -1,4 +1,5 @@
 import {
+  useCallback,
   useContext,
   useEffect,
   useLayoutEffect,
@@ -9,7 +10,10 @@ import {
 // When launch context changes, try to re-run the selector
 // function. If the new selector result !== old selector result
 // then trigger a re-render.
-import { getContextListener } from "../components/LaunchProvider";
+import {
+  getContextListener,
+  useContextListener,
+} from "../components/LaunchProvider";
 import { LaunchContext } from "../types";
 
 // export type Selector = <T>(context: LaunchContext) => T;
@@ -18,24 +22,23 @@ export const useLaunchSelector = <T>(
   selector: (context: LaunchContext) => T
 ): T => {
   const selectedStateRef = useRef();
-  const [selectedState, setSelectedState] = useState<any>();
+  const selectorRef = useRef(selector);
+  const [selectedState, setSelectedState] = useState<any>(() =>
+    selector(getContextListener().snapshot())
+  );
 
   useLayoutEffect(() => {
     selectedStateRef.current = selectedState;
+    selectorRef.current = selector;
   });
 
-  useLayoutEffect(() => {
-    const onContextChange = (context) => {
-      const newState = selector(context);
-      if (newState !== selectedStateRef.current) {
-        setSelectedState(newState);
-      }
-    };
-    const { disconnect } = getContextListener().subscribe(onContextChange);
-
-    return () => {
-      disconnect();
-    };
+  const onContextChange = useCallback((context) => {
+    const newState = selectorRef.current(context);
+    if (newState !== selectedStateRef.current) {
+      setSelectedState(newState);
+    }
   }, []);
+  useContextListener(onContextChange);
+
   return selectedState;
 };
